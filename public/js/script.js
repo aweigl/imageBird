09e6a4e849a0deaf2640279f1c7caf9e7248a3a6
Aaron Weigl <aaron.weigl@hotmail.com>
2018-05-24 13:28:30 +0200

    disabled polling

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,8 @@
-(function() {
+(function () {
     Vue.component("comment-modal", {
         template: "#comment-modal",
         props: ["imageid", "nextimgid", "previmgid"],
-        data: function() {
+        data: function () {
             return {
                 url: "",
                 title: "",
@@ -14,19 +14,21 @@
             };
         },
         watch: {
-            imageid: function() {
+            imageid: function () {
                 this.mounter();
             }
         },
         methods: {
-            mounter: function() {
+            mounter: function () {
                 var app = this;
                 var queryId = this.imageid;
                 axios
                     .get("/commentModal", {
-                        params: { id: queryId }
+                        params: {
+                            id: queryId
+                        }
                     })
-                    .then(function(response) {
+                    .then(function (response) {
                         console.log("COMMENTMODAL", response);
                         if (response.data.success) {
                             var clone = [...response.data.imageData];
@@ -42,14 +44,14 @@
                             app.username = response.data.imageData[0].username;
                         }
                     })
-                    .catch(function(e) {
+                    .catch(function (e) {
                         console.log(e);
                     });
             },
-            close: function() {
+            close: function () {
                 this.$emit("closemodal");
             },
-            commentUpload: function(e) {
+            commentUpload: function (e) {
                 var app = this;
                 var comment = {
                     comment: this.comment.comment,
@@ -58,7 +60,7 @@
                 };
                 axios
                     .post("/comment", comment)
-                    .then(function(comment) {
+                    .then(function (comment) {
                         if (comment.data.success) {
                             var commentclone = comment.data.commentData[0];
 
@@ -68,14 +70,14 @@
                             app.comments.unshift(commentclone);
                         }
                     })
-                    .catch(function(e) {
+                    .catch(function (e) {
                         console.log(e);
                     });
             }
         },
-        mounted: function() {
+        mounted: function () {
             var app = this;
-            window.addEventListener("keyup", function(e) {
+            window.addEventListener("keyup", function (e) {
                 if (e.keyCode === 27) {
                     app.close();
                 }
@@ -87,19 +89,19 @@
 
     new Vue({
         el: "#main",
-        mounted: function() {
+        mounted: function () {
             var app = this;
-            axios.get("/polling").then(function(data) {
-                if (data.data.success) {
-                    app.maxInDb = data.data.max;
-                }
-            });
-            this.polling();
-            addEventListener("hashchange", function() {
+            // axios.get("/polling").then(function (data) {
+            //     if (data.data.success) {
+            //         app.maxInDb = data.data.max;
+            //     }
+            // });
+            // this.polling();
+            addEventListener("hashchange", function () {
                 app.detectId();
             });
             ////////KEYLEFTRIGHT///////////////////
-            window.addEventListener("keyup", function(e) {
+            window.addEventListener("keyup", function (e) {
                 if (e.keyCode === 39) {
                     location.href = `#${app.nextImageId}`;
                 } else if (e.keyCode === 37) {
@@ -109,7 +111,7 @@
             ///////////////////////////////////
             axios
                 .get("/imageboard")
-                .then(function(response) {
+                .then(function (response) {
                     app.lastImgId = response.data.lastImgId;
                     app.picInfo = response.data.data;
                     location.hash && app.detectId();
@@ -120,28 +122,28 @@
         },
         methods: {
             ////////////BASIC AUTHEN/////////////////////
-            adminAcc: function() {
+            adminAcc: function () {
                 var app = this;
                 axios
                     .get("/admin")
-                    .then(function(data) {
+                    .then(function (data) {
                         if (data.data.success) {
                             app.admin = true;
                             console.log("hes an admin, let him through!");
                         }
                     })
-                    .catch(function(e) {
+                    .catch(function (e) {
                         console.log(e);
                     });
             },
-            adminlogout: function() {
+            adminlogout: function () {
                 location.reload();
             },
             //////////////////////////////////////////////////
-            polling: function() {
+            polling: function () {
                 var app = this;
-                setTimeout(function() {
-                    axios.get("/polling").then(function(data) {
+                setTimeout(function () {
+                    axios.get("/polling").then(function (data) {
                         if (data.data.success) {
                             console.log("polling");
                             if (app.maxInDb != data.data.max) {
@@ -153,11 +155,11 @@
                     app.polling();
                 }, 5000);
             },
-            reloadPage: function() {
+            reloadPage: function () {
                 var app = this;
                 axios
                     .get("/imageboard")
-                    .then(function(response) {
+                    .then(function (response) {
                         app.lastImgId = response.data.lastImgId;
                         app.picInfo = response.data.data;
                         location.hash && app.detectId();
@@ -167,46 +169,52 @@
                     });
                 app.uploadHappened = false;
             },
-            detectId: function() {
+            detectId: function () {
                 let app = this;
                 this.currentImageId = location.hash.slice(1);
                 console.log(this.currentImageId);
-                this.prevImageId = this.picInfo.filter(function(e, i, a) {
-                    return a[i + 1]
-                        ? a[i + 1].id === Number(app.currentImageId)
-                        : e;
+                this.prevImageId = this.picInfo.filter(function (e, i, a) {
+                    return a[i + 1] ?
+                        a[i + 1].id === Number(app.currentImageId) :
+                        e;
                 })[0].id;
 
                 this.nextImageId = this.picInfo
-                    .filter(function(e, i, a) {
-                        return a[i - 1]
-                            ? a[i - 1].id === Number(app.currentImageId)
-                            : e;
+                    .filter(function (e, i, a) {
+                        return a[i - 1] ?
+                            a[i - 1].id === Number(app.currentImageId) :
+                            e;
                     })
                     .reverse()[0].id;
             },
-            delPic: function(e) {
+            delPic: function (e) {
                 var picture = e.target.id;
                 var app = this;
                 console.log(picture);
                 axios
-                    .get("/deletePic", { params: { id: picture } })
-                    .then(function(response) {
+                    .get("/deletePic", {
+                        params: {
+                            id: picture
+                        }
+                    })
+                    .then(function (response) {
                         if (response.data.success) {
                             location.reload();
                         }
                     })
-                    .catch(function(e) {
+                    .catch(function (e) {
                         console.log(e);
                     });
             },
-            getMore: function() {
+            getMore: function () {
                 var app = this;
                 axios
                     .get("/moreImages", {
-                        params: { id: app.lastImgId }
+                        params: {
+                            id: app.lastImgId
+                        }
                     })
-                    .then(function(response) {
+                    .then(function (response) {
                         if (
                             response.data.morePics ||
                             response.data.morePics != undefined
@@ -220,15 +228,15 @@
                         console.log(e);
                     });
             },
-            closeModalMain: function() {
+            closeModalMain: function () {
                 this.currentImageId = null;
                 window.location.hash = "";
             },
-            setFile: function(e) {
+            setFile: function (e) {
                 this.tooLarge = null;
                 this.uploadFile.file = e.target.files[0];
             },
-            upload: function() {
+            upload: function () {
                 var formData = new FormData();
                 var app = this;
                 formData.append("file", this.uploadFile.file);
@@ -237,13 +245,13 @@
                 formData.append("username", this.uploadFile.username);
                 axios
                     .post("/upload", formData)
-                    .then(function(response) {
+                    .then(function (response) {
                         if (response.data.success) {
                             app.maxInDb = response.data.max;
                             app.picInfo.unshift(response.data.image);
                         }
                     })
-                    .catch(function(e) {
+                    .catch(function (e) {
                         app.tooLarge = true;
                         console.log(e);
                     });
